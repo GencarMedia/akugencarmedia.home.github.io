1452dac1c2a5103f94e0cf003530ad05879010c0
Gencarmedia <149217218+GencarMedia@users.noreply.github.com>
2024-02-03 14:24:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,160 @@
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-  const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId), nav = document.getElementById(navId)
 
-  toggle.addEventListener('click', () =>{
-      // Add show-menu class to nav menu
-      nav.classList.toggle('show-menu')
+  toggle.addEventListener('click', () => {
+    // Add show-menu class to nav menu
+    nav.classList.toggle('show-menu')
 
-      // Add show-icon to show and hide the menu icon
-      toggle.classList.toggle('show-icon')
+    // Add show-icon to show and hide the menu icon
+    toggle.classList.toggle('show-icon')
   })
 }
 
 showMenu('nav-toggle','nav-menu')
-// a
-const ctx = document.getElementById("revenues");
+// skills
+// const ctx = document.getElementById("revenues");
 
-Chart.defaults.color = "#fff",
-Chart.defaults.font.family = "Open Sans";
+// Chart.defaults.color = "#fff",
+// Chart.defaults.font.family = "Open Sans";
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: [
-      "Web Developer",
-      "Phographer",
-      "Desaigner",
-      "Editing",
-      "Problem Solving",
-      "Communication",
-      "Teamwork",
-      "Crative",
-    ],
-    datasets: [
-      {
-        label: "Revenue",
-        data: [
-          65, 90, 42, 70, 78, 40, 82, 65,
-        ],
-        backgroundColor: "aqua",
-        borderRadius: 6,
-        borderSkipped: false,
-      },
-    ],
-  },
+// new Chart(ctx, {
+  // type: "bar",
+  // data: {
+    // labels: [
+      // "Web Developer",
+      // "Phographer",
+      // "Desaigner",
+      // "Editing",
+      // "Problem Solving",
+      // "Communication",
+      // "Teamwork",
+      // "Crative",
+    // ],
+    // datasets: [
+      // {
+        // label: "Revenue",
+        // data: [
+          // 65, 90, 42, 70, 78, 40, 82, 65,
+        // ],
+        // backgroundColor: "aqua",
+        // borderRadius: 6,
+        // borderSkipped: false,
+      // },
+    // ],
+  // },
   // continuation
 
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: "skills & Professional",
-        padding: {
-          bottom: 16,
-        },
-        font: {
-          size: 16,
-          weight: "normal",
-        },
-      },
-      tooltip: {
-        backgroundColor: "#111",
-      },
-    },
-    scales: {
-      x: {
-        border: {
-          dash: [2, 4],
-        },
-        grid: {
-          color: "#111",
-        },
-        title: {
-          text: "2023",
-        },
-      },
-      y: {
-        grid: {
-          color: "#111",
-        },
-        border: {
-          dash: [2, 4],
-        },
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: "Revenue",
-        },
-      },
-    },
-  },
+  // options: {
+    // responsive: true,
+    // maintainAspectRatio: false,
+    // plugins: {
+      // legend: {
+        // display: false,
+      // },
+      // title: {
+        // display: true,
+        // text: "skills & Professional",
+        // padding: {
+          // bottom: 16,
+        // },
+        // font: {
+          // size: 16,
+          // weight: "normal",
+        // },
+      // },
+      // tooltip: {
+        // backgroundColor: "#111",
+      // },
+    // },
+    // scales: {
+      // x: {
+        // border: {
+          // dash: [2, 4],
+        // },
+        // grid: {
+          // color: "#111",
+        // },
+        // title: {
+          // text: "2023",
+        // },
+      // },
+      // y: {
+        // grid: {
+          // color: "#111",
+        // },
+        // border: {
+          // dash: [2, 4],
+        // },
+        // beginAtZero: true,
+        // title: {
+          // display: true,
+          // text: "Revenue",
+        // },
+      // },
+    // },
+  // },
+// });
+// sert
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
 });
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
 // music
 var player = document.getElementById("player");
 let progress = document.getElementById("progress");
